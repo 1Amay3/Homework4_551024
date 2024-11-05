58ad183956de7c995b6aec5ec63d4a5a1926e194
Navneet Shankar <navneets099@gmail.com>
2024-11-04 19:59:56 -0500

    Changes in Barrier test

--- src/main/java/edu/vt/ece/hw4/barriers/ArrayBarrier.java
+++ src/main/java/edu/vt/ece/hw4/barriers/ArrayBarrier.java
@@ -5,37 +5,36 @@ import edu.vt.ece.spin.ThreadID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ArrayBarrier implements Barrier {
-    private AtomicInteger[] b;
-    private int n;
-    private AtomicInteger count = new AtomicInteger(0);
+    private final AtomicInteger[] b;
+    private final int n;
 
-    public ArrayBarrier( int n) {
+    public ArrayBarrier(int n) {
         this.b = new AtomicInteger[n];
         for (int i = 0; i < n; i++) {
             this.b[i] = new AtomicInteger(0);
         }
         this.n = n;
     }
+
     @Override
     public void enter() {
         int threadId = ThreadID.get();
 
-        if(threadId ==0){
+        if (threadId == 0) {
             b[0].set(1);
-        } else{
-            while(b[threadId-1].get() != 1){
+        } else {
+            while (b[threadId - 1].get() != 1) {
                 Thread.yield();
             }
             b[threadId].set(1);
         }
 
-        if(threadId == n-1){
-            b[n-1].set(2);
-        } else{
-            while(b[n-1].get() !=2){
-
+        if (threadId == n - 1) {
+            b[n - 1].set(2);
+        } else {
+            while (b[n - 1].get() != 2) {
+                Thread.yield();
             }
         }
-
     }
 }
