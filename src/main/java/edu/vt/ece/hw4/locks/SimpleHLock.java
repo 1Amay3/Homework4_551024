9f9b7a6b9996855bc02b6f182dc170cf88bcd03e
Navneet Shankar <navneets099@gmail.com>
2024-11-04 21:32:44 -0500

    Changes in Cluster Test

--- src/main/java/edu/vt/ece/hw4/locks/SimpleHLock.java
+++ src/main/java/edu/vt/ece/hw4/locks/SimpleHLock.java
@@ -9,20 +9,21 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class SimpleHLock implements Lock {
 
     private final Lock globalLock;
-    private final Lock localLocks[];
+    private final Lock[] localLocks;
     private final int clusters;
     private final int BATCH_COUNT;
-    private final AtomicInteger localCount[]; //number of threads waiting per cluster
+    private final AtomicInteger[] localCount; //number of threads waiting per cluster
 
     public SimpleHLock(int clusters,int batchCount) {
         this.globalLock = new TTASLock();
         this.localLocks = new Lock[clusters];
-        for(int i = 0; i < clusters; i++) {
-            localLocks[i] = new TTASLock();
-        }
         this.clusters = clusters;
         this.BATCH_COUNT = batchCount;
         this.localCount = new AtomicInteger[clusters];
+        for(int i = 0; i < clusters; i++) {
+            localLocks[i] = new TTASLock();
+            localCount[i] = new AtomicInteger(0);
+        }
     }
 
     @Override
@@ -30,9 +31,8 @@ public class SimpleHLock implements Lock {
         int cluster = ThreadCluster.getCluster() % clusters;
         localCount[cluster].incrementAndGet();
         localLocks[cluster].lock();
-        localCount[cluster].decrementAndGet();
 
-        if(localCount[cluster].get() == 0){
+        if(localCount[cluster].decrementAndGet() == 0){
             globalLock.lock();
         }
 
@@ -40,7 +40,7 @@ public class SimpleHLock implements Lock {
 
     @Override
     public void unlock() {
-        int cluster = ThreadCluster.getCluster();
+        int cluster = ThreadCluster.getCluster() % clusters;
         if(localCount[cluster].get() ==0 || localCount[cluster].get() % BATCH_COUNT == 0){
             globalLock.unlock();
         }
